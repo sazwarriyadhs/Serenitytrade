4f89b205c13f1c1c1bdb3ff5ec66f8f6c830d478
Azwar Riyadh <sazwarriyadh@gmail.com>
2025-07-07 07:03:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(authenticated)/membership-card/page.tsx
+++ src/app/(authenticated)/membership-card/page.tsx
@@ -1,7 +1,7 @@
 
 'use client'
 
-import { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
