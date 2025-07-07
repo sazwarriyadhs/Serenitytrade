ea6723e2d94d2775188f05683e19f8b1bd3eb28e
Azwar Riyadh <sazwarriyadh@gmail.com>
2025-07-07 01:00:16 +0000

    perbesar ukuran logo, hapus tulisan Serenity AgriExport Hub di header

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,10 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <Image src="/images/logo.png" alt="Serenity AgriExport Hub Logo" width={24} height={24} className="h-6 w-6" />
-      <span className="font-headline text-lg font-semibold">
-        Serenity AgriExport Hub
-      </span>
+      <Image src="/images/logo.png" alt="Serenity AgriExport Hub Logo" width={32} height={32} className="h-8 w-8" />
     </div>
   );
 }
