e17888bd38cb03a7763cb62c1433bb8f8b45bd8a
Azwar Riyadh <sazwarriyadh@gmail.com>
2025-07-08 03:54:10 +0000

    https://6000-firebase-studio-1751839334535.cluster-fkltigo73ncaixtmokrzx

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -121,7 +121,7 @@ export default function AuthenticatedLayout({
             </div>
             <UserNav role={currentUser?.name} email={currentUser?.email} />
           </header>
-          <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          <main className="flex flex-1 flex-col p-4 md:p-6">
             {children}
           </main>
         </SidebarInset>
